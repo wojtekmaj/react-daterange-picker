d6c5b51c5b33732d82e97ea4492ab2099a21843b
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-31 21:19:52 +0200

    Import styles indirectly in Test suite to support simplified CSS building method

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import DateRangePicker from 'react-daterange-picker/src/entry';
+import DateRangePicker from 'react-daterange-picker/src/entry.nostyle';
+import 'react-daterange-picker/src/DateRangePicker.less';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'react-calendar/src/Calendar.less';
 
 import ValidityOptions from './ValidityOptions';
 import MaxDetailOptions from './MaxDetailOptions';
