43ed9e11a38990304ecdf9cb2023dbf337d6527a
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-07-10 09:46:28 +0200

    Fix import/no-unresolved error in entry.js

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,9 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 
 import DateRangePicker from './DateRangePicker';
+
+// File is created during build phase and placed in dist directory
+// eslint-disable-next-line import/no-unresolved
 import './DateRangePicker.css';
 
 export default DateRangePicker;
