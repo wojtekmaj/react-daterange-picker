955ae038e7264a6423a186a15bab3dc5a5207a29
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-03-13 14:25:49 +0100

    Reduce dependency on external libraries (#58)
    * Reduce dependency on external libraries
    
    * Reduce code complexity

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -1,4 +1,39 @@
-export {
-  isMaxDate,
-  isMinDate,
-} from 'react-calendar/dist/shared/propTypes';
+export const isMinDate = (props, propName, componentName) => {
+  const { [propName]: minDate } = props;
+
+  if (!minDate) {
+    return null;
+  }
+
+  if (!(minDate instanceof Date)) {
+    return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+  }
+
+  const { maxDate } = props;
+
+  if (maxDate && minDate > maxDate) {
+    return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, minDate cannot be larger than maxDate.`);
+  }
+
+  return null;
+};
+
+export const isMaxDate = (props, propName, componentName) => {
+  const { [propName]: maxDate } = props;
+
+  if (!maxDate) {
+    return null;
+  }
+
+  if (!(maxDate instanceof Date)) {
+    return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+  }
+
+  const { minDate } = props;
+
+  if (minDate && maxDate < minDate) {
+    return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, maxDate cannot be smaller than minDate.`);
+  }
+
+  return null;
+};
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 
-export { callIfDefined } from 'react-date-picker/dist/shared/utils';
+/**
+ * Calls a function, if it's defined, with specified arguments
+ * @param {Function} fn
+ * @param {Object} args
+ */
+export function callIfDefined(fn, ...args) {
+  if (fn && typeof fn === 'function') {
+    fn(...args);
+  }
+}
