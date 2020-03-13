39e495bed6a12899f8b0b3329330a0572c114a4c
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-03-13 12:56:14 +0100

    Reduce dependency on external libraries

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -1,4 +1,37 @@
-export {
-  isMaxDate,
-  isMinDate,
-} from 'react-calendar/dist/shared/propTypes';
+export const isMinDate = (props, propName, componentName) => {
+  const { [propName]: minDate } = props;
+
+  if (minDate) {
+    if (!(minDate instanceof Date)) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+    }
+
+    const { maxDate } = props;
+
+    if (maxDate && minDate > maxDate) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, minDate cannot be larger than maxDate.`);
+    }
+  }
+
+  // Everything is fine
+  return null;
+};
+
+export const isMaxDate = (props, propName, componentName) => {
+  const { [propName]: maxDate } = props;
+
+  if (maxDate) {
+    if (!(maxDate instanceof Date)) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, expected instance of \`Date\`.`);
+    }
+
+    const { minDate } = props;
+
+    if (minDate && maxDate < minDate) {
+      return new Error(`Invalid prop \`${propName}\` of type \`${typeof maxDate}\` supplied to \`${componentName}\`, maxDate cannot be smaller than minDate.`);
+    }
+  }
+
+  // Everything is fine
+  return null;
+};
---
+++ src/shared/utils.spec.js
@@ -0,0 +1,13 @@
+import { callIfDefined } from './utils';
+
+describe('callIfDefined', () => {
+  it('calls given function if defined', () => {
+    const fn = jest.fn();
+    const arg1 = 'hi';
+    const arg2 = 'hello';
+
+    callIfDefined(fn, arg1, arg2);
+
+    expect(fn).toHaveBeenCalledWith(arg1, arg2);
+  });
+});
