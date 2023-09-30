4e11c1b7725426f5d9c93edfc4de6f8cc9ae9988
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-30 20:57:18 +0200

    Use new JSX transform in all sample and test suites

--- test/Test.tsx
+++ test/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 import '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css';
 import 'react-calendar/dist/Calendar.css';
