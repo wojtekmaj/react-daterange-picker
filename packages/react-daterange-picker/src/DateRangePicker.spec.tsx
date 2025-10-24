01cc0a2441861b670f123349c5dc416c4d0c7165
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-10-24 09:51:16 +0200

    Update Vitest to 4.0.0

--- packages/react-daterange-picker/src/DateRangePicker.spec.tsx
+++ packages/react-daterange-picker/src/DateRangePicker.spec.tsx
@@ -1,5 +1,5 @@
-import { userEvent } from '@vitest/browser/context';
 import { describe, expect, it, vi } from 'vitest';
+import { userEvent } from 'vitest/browser';
 import { act, fireEvent, render, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 
 import DateRangePicker from './DateRangePicker.js';
